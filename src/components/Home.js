5873ec38b4e5af458bf179dafa7f0ba3e5593a8d
cen19 <wilsoncen@gmail.com>
2018-04-10 15:09:15 -0700

    tested hot-reloading, works

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,6 +6,7 @@ import Layout from './Layout';
 const Home = () => {
   return (
     <Layout>
+      <h1>My personal title: hot-loading works! hey</h1>
       <p>Hello World of React and Webpack!</p>
       <p>
         <Link to="/dynamic">Navigate to Dynamic Page</Link>
