18bc82f7a09589b442e87d66ad462ce30b5e8c73
cen19 <wilsoncen@gmail.com>
2018-04-11 02:33:55 -0700

    fixed server to server up static files

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '../dist/'));
+app.use('/', express.static(`${__dirname}/../dist`));
 
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../dist', 'index.html'));
