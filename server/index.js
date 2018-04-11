333fd0ff9ec51c5d017ea4a2550797393dd9f35a
cen19 <wilsoncen@gmail.com>
2018-04-11 02:31:42 -0700

    started server progress

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(__dirname + '../dist/'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../dist', 'index.html'));
+});
+
+app.listen(3000, () => (console.log('Listening on port 3000')));
